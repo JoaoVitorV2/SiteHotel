c2e3bb78f6d5bb229c7c117f22c7a6ad02c30864
João Vitor Viecili <joao_vitor_viecili@outlook.com>
2020-09-04 00:35:22 -0300

    PreLoad added (not tested)

---
+++ preLoad.js
@@ -0,0 +1,10 @@
+let imageCache = {
+	ap: ["imagens/apartamento_cama.jpg", "imagens/apartamento_banheiro_temp.jpg", "imagens/apartamento.jpg", "imagens/apartamento_frigobar.jpg", "imagens/apartamento_ar.jpg"],
+	qt: ["imagens/quarto.jpg", "imagens/quarto_tv.jpg"],
+	cafe: ["imagens/cafe.jpg", "imagens/cafe2.jpg"],
+	janta: ["imagens/janta.jpg", "imagens/janta2.jpg"]
+};
+
+imageCache.ap.forEach(
+	(url) => { var img = new Image(); img.src = url; },
+);
