7461c9f3dab3076cc18c59f4371a1b5959d1ee9f
João Vitor Viecili <joao_vitor_viecili@outlook.com>
2020-09-04 02:40:03 -0300

    PreLoad Improvement #2

--- preLoad.js
+++ preLoad.js
@@ -1,39 +1,38 @@
 var imageCache = (function() {
 	var list = {
+		logo: ["imagens/logo.jpg"],
+		vista: ["imagens/vista.jpg"],
 		ap: ["imagens/apartamento_cama.jpg", "imagens/apartamento_banheiro_temp.jpg", "imagens/apartamento.jpg", "imagens/apartamento_frigobar.jpg", "imagens/apartamento_ar.jpg"],
 		qt: ["imagens/quarto.jpg", "imagens/quarto_tv.jpg"],
 		cafe: ["imagens/cafe.jpg", "imagens/cafe2.jpg"],
 		janta: ["imagens/janta.jpg", "imagens/janta2.jpg"]
 	};
-	var essentials = ["imagens/logo.jpg", "imagens/vista.jpg"];
 
+	var imageCount = 0;
 	var load = function () {
-		for (i = 0; i < essentials.length; i++) {
-			var img = new Image();
-			img.onload = imageCache.checkEssentialsLoaded;
-			img.src = essentials[i];
-		}
-	}
-	var checkEssentialsLoaded = function () {
-		if (typeof checkEssentialsLoaded.count == "undefined") {
-			checkEssentialsLoaded.count = 0;
-		}
-		checkEssentialsLoaded.count++;
-		if (checkEssentialsLoaded.count == essentials.length) {
-			loadRest();
-		}
-	}
-	var loadRest = function () {
 		for (var arr in list) {
-			list[arr].forEach((url) => { var img = new Image(); img.src = url; });
+			list[arr].forEach((url) => {
+				var img = new Image();
+				img.onload = imageCache.checkLoaded;
+				img.src = url;
+				imageCount++;
+			});
+		}
+	}
+	var checkLoaded = function () {
+		if (typeof checkLoaded.count == "undefined") {
+			checkLoaded.count = 0;
+		}
+		checkLoaded.count++;
+		if (checkLoaded.count == imageCount) {
+			document.querySelectorAll(".page").forEach((element) => { element.style.opacity = "1"; });
 		}
 	}
 
 	return {
 		list: list,
 		load: load,
-		checkEssentialsLoaded: checkEssentialsLoaded,
-		loadRest: loadRest
+		checkLoaded: checkLoaded,
 	}
 })();
 
